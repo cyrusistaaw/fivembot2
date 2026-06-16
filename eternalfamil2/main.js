a83c747c0cdbe5a5b728e2dfd10266ed28835641
cyrusistaaw <semihsafhalis@gmail.com>
2026-06-17 02:01:57 +0300

    Update main.js

--- eternalfamil2/main.js
+++ eternalfamil2/main.js
@@ -32,7 +32,7 @@ mainBot.once('ready', async () => {
     console.log(`${colors.green}✅ Ana bot başarıyla giriş yaptı: ${mainBot.user.tag}${colors.reset}`);
 
     // Yayında statüsü ve yazısı
-    mainBot.user.setActivity('Developed By CyrusFix', {
+    mainBot.user.setActivity('Cyrus🤍Beyza', {
         type: ActivityType.Streaming,
         url: 'https://www.twitch.tv/discord'
     });
